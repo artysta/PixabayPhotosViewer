3e9517080a5f121416f2c4e90ebe525714dd517b
Adrian Kurek <adrian.kurek@microsoft.wsei.edu.pl>
2020-05-30 12:21:32 +0200

    Upload initial project

---
+++ app/src/main/java/pl/adriankurek/pixabayphotosviewer/views/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.adriankurek.pixabayphotosviewer.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.adriankurek.pixabayphotosviewer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
