79ada685ec29459bdaa6f1fba99904860592008d
artysta <adrian.kurek93@gmail.com>
2020-06-03 19:48:22 +0200

    Add PhotoViewModel

---
+++ app/src/main/java/pl/adriankurek/pixabayphotosviewer/viewmodels/PhotoViewModel.java
@@ -0,0 +1,33 @@
+package pl.adriankurek.pixabayphotosviewer.viewmodels;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.android.volley.RequestQueue;
+
+import java.util.List;
+
+import pl.adriankurek.pixabayphotosviewer.models.PixabayPhoto;
+import pl.adriankurek.pixabayphotosviewer.models.PixabayPhotosRepository;
+
+public class PhotoViewModel extends ViewModel {
+    private MutableLiveData<List<PixabayPhoto>> photos;
+    private PixabayPhotosRepository repository;
+
+    public void init() {
+        if (photos != null) return;
+
+        repository = PixabayPhotosRepository.getInstance();
+    }
+
+    public void reloadPhotos(Context ctx, String jsonURL, RequestQueue.RequestFinishedListener listener) {
+        photos = repository.getPhotos(ctx, jsonURL, listener);
+    }
+
+    public LiveData<List<PixabayPhoto>> getPhotos() {
+        return photos;
+    }
+}
