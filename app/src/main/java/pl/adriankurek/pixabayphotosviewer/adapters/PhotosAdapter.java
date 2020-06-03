28830b23558ef5c0620510171aceffbe7e7c73a4
artysta <adrian.kurek93@gmail.com>
2020-06-03 16:10:53 +0200

    Add adapter for photos grid

---
+++ app/src/main/java/pl/adriankurek/pixabayphotosviewer/adapters/PhotosAdapter.java
@@ -0,0 +1,59 @@
+package pl.adriankurek.pixabayphotosviewer.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pl.adriankurek.pixabayphotosviewer.R;
+import pl.adriankurek.pixabayphotosviewer.models.PixabayImage;
+
+public class PhotosAdapter extends BaseAdapter {
+    private List<PixabayImage> photos;
+    private Context ctx;
+    private LayoutInflater inflater;
+
+    public PhotosAdapter(Context ctx, List<PixabayImage> photos){
+        this.ctx = ctx;
+        this.photos = photos;
+        this.inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public int getCount(){
+        return photos.size();
+    }
+
+    public Object getItem(int position){
+        return photos.get(position);
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.photos_grid_layout,null);
+            viewHolder = new ViewHolder();
+            viewHolder.txtId = convertView.findViewById(R.id.txt_photo_id);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Get current photo.
+        PixabayImage current = photos.get(position);
+        viewHolder.txtId.setText(String.valueOf(current.getId()));
+        return convertView;
+    }
+
+    private static class ViewHolder{
+        TextView txtId;
+    }
+}
