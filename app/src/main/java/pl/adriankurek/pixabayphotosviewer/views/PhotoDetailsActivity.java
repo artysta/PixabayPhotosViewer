ce60445d1dffc1ce06e8b2a2c470c60fabd2ed08
artysta <adrian.kurek93@gmail.com>
2020-06-04 20:30:55 +0200

    Add code that allows to get info about selected photo

--- app/src/main/java/pl/adriankurek/pixabayphotosviewer/views/PhotoDetailsActivity.java
+++ app/src/main/java/pl/adriankurek/pixabayphotosviewer/views/PhotoDetailsActivity.java
@@ -2,15 +2,60 @@ package pl.adriankurek.pixabayphotosviewer.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.squareup.picasso.Picasso;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+import pl.adriankurek.pixabayphotosviewer.BuildConfig;
 import pl.adriankurek.pixabayphotosviewer.R;
+import pl.adriankurek.pixabayphotosviewer.models.JSONHelper;
+import pl.adriankurek.pixabayphotosviewer.models.PixabayPhoto;
 
 public class PhotoDetailsActivity extends AppCompatActivity {
+    private final String API_KEY = BuildConfig.PIXABAY_API;
+
+    private PixabayPhoto photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_details);
+
+        // Get selected photo id.
+        Intent intent = getIntent();
+        String photoId = intent.getStringExtra("PHOTO_ID");
+
+        String photoURL = MessageFormat.format("https://pixabay.com/api/?key={0}&id={1}", API_KEY, photoId);
+
+        JSONHelper helper = new JSONHelper(this);
+        List<PixabayPhoto> photos = helper.getImagesFromJSONURL(photoURL);
+
+        helper.addRequestFinishedListener(r -> {
+            photo = photos.get(0);
+            setPhotoDetails();
+        });
+    }
+
+    public void setPhotoDetails() {
+        ImageView imgView = findViewById(R.id.imgView);
+        TextView txtId = findViewById(R.id.txt_photo_id);
+        TextView txtWebviewURL = findViewById(R.id.txt_webview_url);
+        TextView txtUser = findViewById(R.id.txt_user);
+        TextView txtTags = findViewById(R.id.txt_tags);
+
+        Picasso.get().load(photo.getWebformatURL()).into(imgView);
+        txtId.setText(String.valueOf(photo.getId()));
+        txtWebviewURL.setText(photo.getWebformatURL());
+        txtUser.setText(photo.getUser());
+        txtTags.setText(photo.getTags());
     }
 }
