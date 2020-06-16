ec900f2d627a8413f18f7da0212d3a5ddb0932a6
artysta <adrian.kurek93@gmail.com>
2020-06-16 20:10:34 +0200

    Add greendao annotations

--- app/src/main/java/pl/adriankurek/pixabayphotosviewer/models/PixabayPhoto.java
+++ app/src/main/java/pl/adriankurek/pixabayphotosviewer/models/PixabayPhoto.java
@@ -2,10 +2,16 @@ package pl.adriankurek.pixabayphotosviewer.models;
 
 import androidx.annotation.NonNull;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+
 // Class that describes images from pixabay.com (with just 2 fields at this moment)
 // TODO add rest of fields
+@Entity(nameInDb = "photos")
 public class PixabayPhoto {
-    private int id;
+    @Id
+    private long id;
     private String previewURL;
 
     // Tags can be splitted to array by comma.
@@ -13,11 +19,24 @@ public class PixabayPhoto {
     private String webformatURL;
     private String user;
 
-    public void setId(int id) {
+    @Generated(hash = 371426933)
+    public PixabayPhoto(long id, String previewURL, String tags, String webformatURL, String user) {
+        this.id = id;
+        this.previewURL = previewURL;
+        this.tags = tags;
+        this.webformatURL = webformatURL;
+        this.user = user;
+    }
+
+    @Generated(hash = 578507231)
+    public PixabayPhoto() {
+    }
+
+    public void setId(long id) {
         this.id = id;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -58,4 +77,5 @@ public class PixabayPhoto {
     public String toString() {
         return String.format("[%s %s] previewURL: %s.", this.getClass().getSimpleName(), id, previewURL);
     }
+
 }
